4e647dae49a1abb59ddb655bdef68fbd8c957b51
Philipp Ittershagen <p.ittershagen@mailbox.org>
2022-12-05 22:42:34 +0100

    Clean up day 5 solution

--- src/advent_of_code/2022/day5.clj
+++ src/advent_of_code/2022/day5.clj
@@ -13,10 +13,6 @@
              8 (reverse '(C G Q J R))
              9 (reverse '(S F P H R T D L))})
 
-(def testcrates {1 '(Z N)
-                 2 '(M C D)
-                 3 '(P)})
-
 ; return the items (in same order) from the crate
 (defn peek-crate [n num crate]
   (take-last num (crate n)))
@@ -29,40 +25,50 @@
 ; push the items (in reverse order) to the crate, return the new crate
 (defn push-crate [n items crate]
   (assoc crate n
+         (concat (crate n) (reverse items))))
+
+; push the items (in same order) to the crate, return the new crate
+(defn push-crate-star2 [n items crate]
+  (assoc crate n
          (concat (crate n) items)))
 
+; move num crates from stack 'from' to 'to' with the help of a pusher-fn 'f'
 (defn move
-  [[num from to] crate]
-  (->> crate
-       (push-crate to (peek-crate from num crate))
+  [f stacks [num from to]]
+  (->> stacks
+       (f to (peek-crate from num stacks))
        (pop-crate from num)))
 
-(defn create-code [s]
+(defn create-mover [f]
+  (fn
+    ([stacks] stacks)
+    ([stacks instr] (move f stacks instr))))
+
+; get all top crates from the stacks
+(defn top-crates [stacks]
+  (->>
+   (seq (into (sorted-map) stacks))
+   (map (comp last last))))
+
+(def star1 (create-mover push-crate))
+(def star2 (create-mover push-crate-star2))
+
+(defn parse-instr [s]
   (read-string
    (str "("
         (clojure.string/replace s #"[a-z]" "") ")")))
 
-(defn run-moves
-  ([crate] (crate))
-  ([crate instr] (move instr crate))
-  ([crate instr & rest]
-   (apply run-moves (move instr crate) rest)))
+(defn read-instr [fname]
+  (->> (filter #(str/starts-with? % "move")
+               (utils/input->lines fname))
+       (map parse-instr)))
 
-(run-moves testcrates
-           (create-code "move 1 from 2 to 1")
-           (create-code "move 3 from 1 to 3")
-           (create-code "move 2 from 2 to 1")
-           (create-code "move 1 from 1 to 2"))
+; star1
+(->> (read-instr "2022/day5.txt")
+     (reduce star1 crates)
+     (top-crates))
 
-; move 1 from 2 to 1
-; move 3 from 1 to 3
-; move 2 from 2 to 1
-; move 1 from 1 to 2
-
-(map last
-     (map last
-          (seq (into (sorted-map)
-                     (reduce run-moves crates
-                             (->> (filter #(str/starts-with? % "move")
-                                          (utils/input->lines "2022/day5.txt"))
-                                  (map create-code)))))))
+; star2
+(->> (read-instr "2022/day5.txt")
+     (reduce star2 crates)
+     (top-crates))
